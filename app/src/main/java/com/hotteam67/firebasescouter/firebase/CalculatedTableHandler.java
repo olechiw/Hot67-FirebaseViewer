1df22088152bef13998eedd3d0fe36a4afda50b5
Pianist1119 <pianist1119@gmail.com>
2018-01-22 19:54:51 -0500

    Switched to api 24+, used java "linq" to finish (supposedly) calculated stuff

--- app/src/main/java/com/hotteam67/firebasescouter/firebase/CalculatedTableHandler.java
+++ app/src/main/java/com/hotteam67/firebasescouter/firebase/CalculatedTableHandler.java
@@ -1,12 +1,16 @@
 package com.hotteam67.firebasescouter.firebase;
 
+import android.util.Log;
+
 import com.hotteam67.firebasescouter.tableview.tablemodel.CellModel;
 import com.hotteam67.firebasescouter.tableview.tablemodel.ColumnHeaderModel;
 import com.hotteam67.firebasescouter.tableview.tablemodel.RowHeaderModel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Jakob on 1/19/2018.
@@ -19,6 +23,7 @@ public class CalculatedTableHandler {
 
     private DataTableProcessor calculatedDataTable;
     private HashMap<String, Integer> calculatedColumnsDataTableColumns;
+    private List<Integer> calculatedColumnIndices;
 
     public final static class Calculation
     {
@@ -33,22 +38,153 @@ public class CalculatedTableHandler {
         columnsNames = rawData.GetColumnNames();
     }
 
-    public CalculatedTableHandler(DataTableProcessor rawData, HashMap<String, Integer> calculatedColumns)
+    public CalculatedTableHandler(DataTableProcessor rawData, HashMap<String, Integer> calculatedColumns,
+                                  List<Integer> columnIndices)
     {
         rawDataTable = rawData;
         columnsNames = rawData.GetColumnNames();
+        calculatedColumnIndices = columnIndices;
 
-        List<String> teamNumbers = new ArrayList<>();
+        SetupCalculatedColumns(calculatedColumns);
+    }
 
+    public void SetupCalculatedColumns(HashMap<String, Integer> calculatedColumns)
+    {
         List<ColumnHeaderModel> calcColumnHeaders = new ArrayList<>();
         List<List<CellModel>> calcCells = new ArrayList<>();
         List<RowHeaderModel> calcRowHeaders = new ArrayList<>();
 
+        /*
+        Load calculated column names
+         */
         calculatedColumnsDataTableColumns = calculatedColumns;
         for (HashMap.Entry<String, Integer> entry : calculatedColumns.entrySet())
         {
             String calculatedName = getCalculatedColumnName(entry.getKey(), entry.getValue());
             calcColumnHeaders.add(new ColumnHeaderModel(calculatedName));
+        }
+
+        /*
+        Load every unique team number
+         */
+        List<String> teamNumbers = new ArrayList<>();
+
+        // Basically linq, query all of the distinct team numbers and store them
+        teamNumbers.addAll(rawDataTable.GetCells().stream()
+                .map(x -> x.get(0).getContent().toString())
+                .distinct().collect(Collectors.toList()));
+
+        /*
+        Create a calculated row for each teamnumber
+         */
+        int current_row = 0;
+        for (String teamNumber : teamNumbers)
+        {
+            // Get all matches for team number
+            List<List<CellModel>> matches = rawDataTable.GetCells().stream()
+                    .filter(x -> x.get(0).getContent().toString() == teamNumber)
+                    .collect(Collectors.toList());
+
+            List<CellModel> row = new ArrayList<>();
+            int currentCalculatedColumn = 0;
+            int current_column = 0;
+            for (HashMap.Entry<String, Integer> entry : calculatedColumns.entrySet())
+            {
+                int rawColumnIndex = calculatedColumnIndices.get(currentCalculatedColumn);
+                currentCalculatedColumn++;
+
+                List<String> values = new ArrayList<>();
+                values.addAll(matches.stream().map(x ->
+                        /*
+                        Get the calculated column index, and use it to create a list of all of the
+                        values for this team. The indices correspond with the entry for calculation
+                        type
+                         */
+                        x.get(rawColumnIndex)
+                                .getContent().toString()
+                ).collect(Collectors.toList()));
+
+                // Calculate
+                Double value = doCalculatedColumn(columnsNames.get(rawColumnIndex), values, entry.getValue());
+                // Add cell to row
+                row.add(new CellModel(current_row + "_" + current_column, value.toString()));
+
+                current_column++;
+            }
+            // Add row to calculated list
+            calcCells.add(row);
+            calcRowHeaders.add(new RowHeaderModel(teamNumber));
+
+            current_row++;
+        }
+    }
+
+    public static double doCalculatedColumn(String columnName, List<String> columnValues,
+                                                 int calculation)
+    {
+        switch (calculation)
+        {
+            case Calculation.AVERAGE:
+            {
+                try
+                {
+                    return columnValues.stream()
+                            // Convert to number
+                            .mapToDouble(CalculatedTableHandler::SafeConvert)
+                            .average().getAsDouble();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    Log.e("FirebaseScouter",
+                            "Failed to do average calculation on column: " + columnName);
+                }
+                break;
+            }
+            case Calculation.MAXIMUM:
+                try
+                {
+                    return columnValues.stream()
+                            // Convert to number
+                            .mapToDouble(CalculatedTableHandler::SafeConvert)
+                            .max().getAsDouble();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    Log.e("FirebaseScouter",
+                            "Failed to do max calculation on column: " + columnName);
+                }
+                break;
+            case Calculation.MINIMUM:
+                try
+                {
+                    return columnValues.stream()
+                            // Convert to number
+                            .mapToDouble(CalculatedTableHandler::SafeConvert)
+                            .min().getAsDouble();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    Log.e("FirebaseScouter",
+                            "Failed to do max calculation on column: " + columnName);
+                }
+                break;
+        }
+        return 0;
+    }
+
+    public static double SafeConvert(String value)
+    {
+        try
+        {
+            return Double.valueOf(value);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return 0;
         }
     }
 
